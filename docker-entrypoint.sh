36092ae5011ccf0381339afb2ef602dd0a3ac31c
hamzaiqbal35 <hamzaiqbalrajpoot35@gmail.com>
2026-07-17 21:02:29 +0500

    fix docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,15 +7,15 @@ echo "Starting Docker Entrypoint..."
 echo "Waiting for database..."
 sleep 5
 
+# Run Migrations first so the cache table exists
+echo "Running database migrations..."
+php artisan migrate --force
+
 # Clear Laravel caches
 echo "Clearing caches..."
 php artisan config:clear
 php artisan cache:clear
 php artisan view:clear
-
-# Run Migrations
-echo "Running database migrations..."
-php artisan migrate --force
 
 # Start the main process (Apache)
 echo "Starting web server..."
