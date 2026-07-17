dfcae2985c843736f368089130865465f164fab8
hamzaiqbal35 <hamzaiqbalrajpoot35@gmail.com>
2026-07-17 21:48:11 +0500

    Trying to catch the error of credentials

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/init-seeder', function () {
             return 'Database seeded successfully! You can now go to the login page and use admin@example.com / password.';
         }
         return 'Database is already seeded! This route has been deactivated for security.';
-    } catch (\Exception $e) {
+    } catch (\Throwable $e) {
         return 'Error during seeding: ' . $e->getMessage() . '<br><br><pre>' . $e->getTraceAsString() . '</pre>';
     }
 });
